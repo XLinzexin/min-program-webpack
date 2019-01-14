1c17b8365769c6b385ec06c5b60df65df91b83d2
xlinzexin <1046110267@qq.com>
2019-01-14 17:13:15 +0800

    +

--- src/pages/main/home/index.js
+++ src/pages/main/home/index.js
@@ -1,32 +1,13 @@
 const { ajax } = getApp();
 Page({
-	data: {
-		details: {
-			topicTheme: [1],
-			historyOrder: [],
-			classificationList: [],
-			weekSale: []
-		}
+	data: {},
+	async prevLoad() {
+		const res = await ajax.get("/homeData");
+		this.emitData(res.data);
 	},
 	onLoad() {
-		this.getWxDecoration();
-	},
-	onAppInit(appConfig) {
-		const { storeName } = appConfig;
-		wx.setNavigationBarTitle({ title: storeName });
-	},
-	//获取小程序装修接口数据
-	async getWxDecoration() {
-		const res = await ajax.get("/v1/wxDecoration");
-		if (!res.code) {
-			this.setData({
-				details: res.data
-			});
-		}
-	},
-	async toUser() {
-		wx.navigateTo({
-			url: "/pages/main/user/index"
+		this.receiveData(data => {
+			console.log(data);
 		});
 	}
 });
